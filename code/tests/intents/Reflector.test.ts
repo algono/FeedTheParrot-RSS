d80f26416029b96ad6bbb5f269726809d2d60a44
Alejandro Gómez <algono@inf.upv.es>
2020-10-18 23:14:46 +0200

    Improved reflector test intent name generator Now it can only generate valid characters so it won't be filtering out lots of strings with invalid characters still needs to filter out strings with invalid structure though It should be more efficient this way

--- code/tests/intents/Reflector.test.ts
+++ code/tests/intents/Reflector.test.ts
@@ -14,6 +14,14 @@ testIntentCanHandle({
   testName: 'Intent reflector can handle any IntentRequest',
 });
 
+const alphaAndUnderscoreProperty = fc.stringOf(
+  fc.mapToConstant(
+    { num: 26, build: (v) => String.fromCharCode(v + 0x41) }, // A-Z
+    { num: 26, build: (v) => String.fromCharCode(v + 0x61) }, // a-z
+    { num: 1, build: () => '_' } // _
+  )
+);
+
 testInAllLocales(
   'Intent reflector response contains the reflected intent',
   async (locale) => {
@@ -24,13 +32,15 @@ testInAllLocales(
         /**
          * Allow only strings that match an Alexa intent name's restrictions:
          * (the following text was extracted from Alexa prompts, except for the remarks in parenthesis, which are extrapolated from examples)
-         * 
+         *
          * "Intent name cannot be empty, it can only contain case-insensitive alphabetical characters and underscores,
          * and it must begin and end with an alphabetic character. (also no double underscores)
          * Numbers, spaces, or other special characters are not allowed."
-         * (built-in intents have letters and a dot prepended to its name)
+         * (built-in intents have 'AMAZON' and a dot prepended to its name)
          */
-        fc.string({ minLength: 1 }).filter((str) => /^(?:[a-z]+\.)?[a-z]+(?:_[a-z]+)*$/i.test(str)),
+        alphaAndUnderscoreProperty.filter((str) =>
+          /^[a-z]+(?:_[a-z]+)*$/i.test(str)
+        ).map(name => (Math.random() < 0.5 ? 'AMAZON.' : '') + name), // 50/50 of being a built-in intent
         (intentName) => {
           mocked(getIntentName).mockReturnValue(intentName);
 
