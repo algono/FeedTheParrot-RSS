0028f2049bc963db49f1a770a824a300cc07bab5
Alejandro Gómez <algono@inf.upv.es>
2020-11-09 14:35:26 +0100

    Fix reflector test arbitrary It was using Math.random, and that was a mistake because each value must be reproducible in order to be properly shrunk And the Math.random prevented that (I originally used it because I didn't know of an alternative)

--- code/tests/intents/Reflector.test.ts
+++ code/tests/intents/Reflector.test.ts
@@ -22,18 +22,23 @@ testInAllLocales(
 
     fc.assert(
       fc.property(
-        /**
-         * Allow only strings that match an Alexa intent name's restrictions:
-         * (the following text was extracted from Alexa prompts, except for the remarks in parenthesis, which are extrapolated from examples)
-         *
-         * "Intent name cannot be empty, it can only contain case-insensitive alphabetical characters and underscores,
-         * and it must begin and end with an alphabetic character. (also no double underscores)
-         * Numbers, spaces, or other special characters are not allowed."
-         * (built-in intents have 'AMAZON' and a dot prepended to its name)
-         */
-        alphaAndUnderscoreString
-          .filter((str) => /^[a-z]+(?:_[a-z]+)*$/i.test(str))
-          .map((name) => (Math.random() < 0.5 ? 'AMAZON.' : '') + name), // 50/50 of being a built-in intent
+        fc
+          .tuple(
+            fc.option(fc.constant('AMAZON.'), { freq: 2 }), // 50/50 of being a built-in intent
+            /**
+             * Allow only strings that match an Alexa intent name's restrictions:
+             * (the following text was extracted from Alexa prompts, except for the remarks in parenthesis, which are extrapolated from examples)
+             *
+             * "Intent name cannot be empty, it can only contain case-insensitive alphabetical characters and underscores,
+             * and it must begin and end with an alphabetic character. (also no double underscores)
+             * Numbers, spaces, or other special characters are not allowed."
+             * (built-in intents have 'AMAZON' and a dot prepended to its name)
+             */
+            alphaAndUnderscoreString.filter((str) =>
+              /^[a-z]+(?:_[a-z]+)*$/i.test(str)
+            )
+          )
+          .map(([prefix, name]) => (prefix ? prefix + name : name)),
         (intentName) => {
           mocked(getIntentName).mockReturnValue(intentName);
 
