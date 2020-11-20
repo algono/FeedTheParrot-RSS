8bda7ab549d6fdf335992e851542873d6fa4ca28
Alejandro Gómez <algono@inf.upv.es>
2020-11-20 21:26:40 +0100

    Clear session attributes on session end Also added a test for it

--- code/src/intents/SessionEnded.ts
+++ code/src/intents/SessionEnded.ts
@@ -10,6 +10,9 @@ export const SessionEndedRequestHandler: RequestHandler = {
   handle(handlerInput) {
     // Any cleanup logic goes here.
 
+    // Clearing session attributes at session end
+    handlerInput.attributesManager.setSessionAttributes({});
+
     // Clearing dynamic entities at session end (this is a best practice)
     const clearEntitiesDirective: dialog.DynamicEntitiesDirective = {
       type: 'Dialog.UpdateDynamicEntities',
--- code/tests/intents/SessionEnded.test.ts
+++ code/tests/intents/SessionEnded.test.ts
@@ -1,4 +1,4 @@
-import { anyString, anything, capture, verify } from 'ts-mockito';
+import { anyString, anything, capture, deepEqual, verify } from 'ts-mockito';
 import { SessionEndedRequestHandler } from '../../src/intents/SessionEnded';
 import { mockHandlerInput } from '../helpers/mocks/HandlerInputMocks';
 import { testCanHandle, testInAllLocales } from '../helpers/helperTests';
@@ -10,40 +10,41 @@ describe('Session ended request handler', () => {
   testCanHandle({
     requestType: 'SessionEndedRequest',
     handler: SessionEndedRequestHandler,
-    testName:
-      'can handle any SessionEndedRequest',
+    testName: 'can handle any SessionEndedRequest',
   });
 
-  testInAllLocales('clears dynamic entities')(
-    async (locale) => {
-      const mocks = await mockHandlerInput({ locale });
+  testInAllLocales('clears session attributes')(async (locale) => {
+    const mocks = await mockHandlerInput({ locale });
 
-      SessionEndedRequestHandler.handle(mocks.instanceHandlerInput);
+    SessionEndedRequestHandler.handle(mocks.instanceHandlerInput);
 
-      const [directive] = capture(
-        mocks.mockedResponseBuilder.addDirective
-      ).last();
+    verify(mocks.mockedAttributesManager.setSessionAttributes(deepEqual({}))).once();
+  });
 
-      expect(directive).toHaveProperty('type', 'Dialog.UpdateDynamicEntities');
-      expect(directive).toHaveProperty<er.dynamic.UpdateBehavior>(
-        'updateBehavior',
-        'CLEAR'
-      );
-    }
-  );
+  testInAllLocales('clears dynamic entities')(async (locale) => {
+    const mocks = await mockHandlerInput({ locale });
 
-  testInAllLocales('does not speak nor reprompt')(
-    async (locale) => {
-      const mocks = await mockHandlerInput({ locale });
+    SessionEndedRequestHandler.handle(mocks.instanceHandlerInput);
 
-      SessionEndedRequestHandler.handle(mocks.instanceHandlerInput);
+    const [directive] = capture(
+      mocks.mockedResponseBuilder.addDirective
+    ).last();
 
-      verify(
-        mocks.mockedResponseBuilder.speak(anyString(), anything())
-      ).never();
-      verify(
-        mocks.mockedResponseBuilder.reprompt(anyString(), anything())
-      ).never();
-    }
-  );
+    expect(directive).toHaveProperty('type', 'Dialog.UpdateDynamicEntities');
+    expect(directive).toHaveProperty<er.dynamic.UpdateBehavior>(
+      'updateBehavior',
+      'CLEAR'
+    );
+  });
+
+  testInAllLocales('does not speak nor reprompt')(async (locale) => {
+    const mocks = await mockHandlerInput({ locale });
+
+    SessionEndedRequestHandler.handle(mocks.instanceHandlerInput);
+
+    verify(mocks.mockedResponseBuilder.speak(anyString(), anything())).never();
+    verify(
+      mocks.mockedResponseBuilder.reprompt(anyString(), anything())
+    ).never();
+  });
 });
