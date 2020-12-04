52efca60c1e7e76cf95bd032d549b866d034892a
Alejandro Gómez <algono@inf.upv.es>
2020-12-04 20:34:07 +0100

    Auth code length test now uses the new constant Instead of statically checking for a 6 digit code, now it checks that its length equals the constant

--- code/tests/intents/Auth.test.ts
+++ code/tests/intents/Auth.test.ts
@@ -4,6 +4,7 @@ import { testIntentCanHandle } from '../helpers/helperTests';
 
 import { anything, capture, verify } from 'ts-mockito';
 import { mockDatabase } from '../helpers/mocks/mockDatabase';
+import { AUTH_CODE_LENGTH } from '../../src/util/constants';
 
 jest.mock('ask-sdk-core');
 
@@ -13,7 +14,7 @@ describe('Auth intent', () => {
     intentName: 'AuthIntent',
   });
 
-  test('generates a 6 digit code', async () => {
+  test('generates a code with a number of digits equal to the "AUTH_CODE_LENGTH" constant', async () => {
     const mocks = await mockHandlerInput({ locale: null });
 
     const mockedDatabase = mockDatabase();
@@ -24,6 +25,6 @@ describe('Auth intent', () => {
 
     const [authCode] = capture(mockedDatabase.setAuthCode).last();
 
-    expect(authCode.code).toMatch(/[0-9]{6}/);
+    expect(authCode.code).toMatch(new RegExp(`[0-9]{${AUTH_CODE_LENGTH}}`));
   });
 });
