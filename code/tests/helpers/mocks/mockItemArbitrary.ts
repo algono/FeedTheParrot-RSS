7d455866ba75f7685508bc90a2591554c1026a66
Alejandro Gómez <algono@inf.upv.es>
2021-06-25 17:17:38 +0200

    Fixed undefined enclosures in feed item test

--- code/tests/helpers/mocks/mockItemArbitrary.ts
+++ code/tests/helpers/mocks/mockItemArbitrary.ts
@@ -1,15 +1,23 @@
 import fc from 'fast-check';
-import { Item } from 'feedparser';
-import { instance, mock, when } from 'ts-mockito';
+import { Item, Enclosure } from 'feedparser';
+import { anything, instance, mock, when } from 'ts-mockito';
 import { MAX_CHARACTERS_SPEECH } from '../../../src/util/constants';
 import { MayHappenOption } from '../fast-check/arbitraries/misc';
 import { mockArbitrary } from '../fast-check/arbitraries/mockArbitrary';
 
+const hasAudioContentArbs : { readonly [key in MayHappenOption]: fc.Arbitrary<boolean> } = {
+  'always': fc.constant(true),
+  'sometimes': fc.boolean(),
+  'never': fc.constant(false),
+};
+
 export function mockItemArbitrary({
   contentSurpassesMaxCharacters = 'sometimes',
-}: { contentSurpassesMaxCharacters?: MayHappenOption } = {}): fc.Arbitrary<
-  Item
-> {
+  hasAudioContent = 'sometimes',
+}: {
+  contentSurpassesMaxCharacters?: MayHappenOption;
+  hasAudioContent?: MayHappenOption;
+} = {}): fc.Arbitrary<Item> {
   return fc
     .tuple(
       fc.integer({
@@ -22,9 +30,10 @@ export function mockItemArbitrary({
             ? MAX_CHARACTERS_SPEECH
             : undefined,
       }),
-      fc.boolean()
+      fc.boolean(),
+      hasAudioContentArbs[hasAudioContent]
     )
-    .chain(([contentLength, hasSummary]) => {
+    .chain(([contentLength, hasSummary, hasAudio]) => {
       const contentMock = mock<string>();
       when(contentMock.length).thenReturn(contentLength);
       const content = instance(contentMock);
@@ -32,6 +41,12 @@ export function mockItemArbitrary({
       return mockArbitrary<Item>((itemMock) => {
         when(itemMock.summary).thenReturn(hasSummary ? content : null);
         when(itemMock.description).thenReturn(content);
+
+        const enclosuresMock = mock<Enclosure[]>();
+        const enclosureMock = mock<Enclosure>();
+        when(enclosuresMock.find(anything())).thenCall(() => hasAudio ? instance(enclosureMock) : undefined);
+
+        when(itemMock.enclosures).thenCall(() => instance(enclosuresMock));
       });
     });
 }
