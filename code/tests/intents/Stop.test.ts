992ec42a4b78867c3dbb0d48c03e60f4a3fe4dda
Alejandro Gómez <algono@inf.upv.es>
2021-06-27 10:47:21 +0200

    100% coverage for Stop tests

--- code/tests/intents/Stop.test.ts
+++ code/tests/intents/Stop.test.ts
@@ -1,5 +1,7 @@
+import { anything, verify } from 'ts-mockito';
 import { CancelAndStopIntentHandler } from '../../src/intents/Stop';
 import { testIntentCanHandle } from '../helpers/helperTests';
+import { mockHandlerInput } from '../helpers/mocks/HandlerInputMocks';
 
 jest.mock('ask-sdk-core');
 
@@ -11,5 +13,27 @@ describe('Cancel and stop intent', () => {
   testIntentCanHandle({
     handler: CancelAndStopIntentHandler,
     intentName: 'AMAZON.StopIntent',
+  });
+  testIntentCanHandle({
+    handler: CancelAndStopIntentHandler,
+    intentName: 'AMAZON.PauseIntent',
+  });
+
+  test('It stops any currently playing audio', async () => {
+    const { instanceHandlerInput, mockedResponseBuilder } =
+      await mockHandlerInput();
+
+    CancelAndStopIntentHandler.handle(instanceHandlerInput);
+
+    verify(mockedResponseBuilder.addAudioPlayerStopDirective()).once();
+  });
+
+  test('It ends the current skill session', async () => {
+    const { instanceHandlerInput, mockedResponseBuilder } =
+      await mockHandlerInput();
+
+    CancelAndStopIntentHandler.handle(instanceHandlerInput);
+
+    verify(mockedResponseBuilder.withShouldEndSession(anything())).once();
   });
 });
