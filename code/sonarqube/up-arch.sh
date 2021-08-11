d88ad6ec2f5480e5aa6c394164e9893862413739
algono <algono@inf.upv.es>
2021-08-11 14:25:38 +0200

    Unified sonarqube up scripts

--- code/sonarqube/up-arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sonarqube_vm_max_map_count=262144
-current_vm_max_map_count="$(sysctl vm.max_map_count --values)"
-
-[ $current_vm_max_map_count -lt $sonarqube_vm_max_map_count ] && sudo sysctl -w vm.max_map_count=$sonarqube_vm_max_map_count
-
-sudo docker-compose up -d
--- code/sonarqube/up.sh
+++ code/sonarqube/up.sh
@@ -4,4 +4,10 @@ current_vm_max_map_count="$(sysctl vm.max_map_count --values)"
 
 [ $current_vm_max_map_count -lt $sonarqube_vm_max_map_count ] && sudo sysctl -w vm.max_map_count=$sonarqube_vm_max_map_count
 
-docker-compose up -d
+# If the current user is a member of the 'docker' group, sudo is not needed
+if id -Gn | grep -q '\bdocker\b'
+then
+  docker-compose up -d
+else
+  sudo docker-compose up -d
+fi
