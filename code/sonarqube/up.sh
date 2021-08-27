aecfbe24952f79dc51e4606444111323a3b7bf5f
algono <algono@inf.upv.es>
2021-08-27 19:35:14 +0200

    Updated sonarqube vm.max_map_count Turns out that from v8.4 onward, the min value was doubled (at least in their documentation)

--- code/sonarqube/up.sh
+++ code/sonarqube/up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-sonarqube_vm_max_map_count=262144
+# Versions 8.4 or later
+sonarqube_vm_max_map_count=524288
+# Versions 8.3 or earlier
+#sonarqube_vm_max_map_count=262144
 current_vm_max_map_count="$(sysctl vm.max_map_count --values)"
 
 [ $current_vm_max_map_count -lt $sonarqube_vm_max_map_count ] && sudo sysctl -w vm.max_map_count=$sonarqube_vm_max_map_count
